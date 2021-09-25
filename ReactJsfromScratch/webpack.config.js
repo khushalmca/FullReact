0e35130868001027cfb9f23f17453e0ee05f8c64
khushalmca <87666685+khushalmca@users.noreply.github.com>
2021-09-25 23:30:55 +0530

    Initial Upload

---
+++ ReactJsfromScratch/webpack.config.js
@@ -0,0 +1,31 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const htmlPlugin = new HtmlWebPackPlugin({
+ template: "./src/index.html",
+ filename: "./index.html"
+});
+module.exports = {
+mode: "development",
+entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  module: {
+    rules: [{
+   test: /\.js$/,
+   exclude: /node_modules/,
+   use: {
+     loader: "babel-loader"
+   }
+ },
+  {
+   test: /\.css$/,
+   use: ["style-loader", "css-loader"]
+  }
+]},
+ plugins: [htmlPlugin]
+};
